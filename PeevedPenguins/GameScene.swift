8ea557be095f52105d86472de2fbe2841895d981
calee14 <caplee1000@gmail.com>
2017-03-28 22:01:48 -0700

    Added Levels, clamped camera, etc.

--- PeevedPenguins/GameScene.swift
+++ PeevedPenguins/GameScene.swift
@@ -14,10 +14,50 @@ class GameScene: SKScene {
     //game object connnections
     var catapultArm: SKSpriteNode!
     
+    //Level loader holder
+    var levelNode: SKNode!
+    
+    //Camera helpers
+    var cameraTarget: SKNode?
+    
+    //UI Connections
+    var buttonRestart: MSButtonNode!
+    
     override func didMove(to view: SKView) {
         //Set reference to catapultArm node
         catapultArm = childNode(withName: "catapultArm") as! SKSpriteNode
         
+        //Set reference to the level loader node
+        levelNode = childNode(withName: "//levelNode")
+        
+        //Load Level 1
+        let resourcePath = Bundle.main.path(forResource: "Level1", ofType: "sks")
+        let newLevel = SKReferenceNode(url: NSURL(fileURLWithPath: resourcePath!) as URL)
+        levelNode.addChild(newLevel)
+        
+        //UI Connections
+        buttonRestart = childNode(withName: "//buttonRestart") as! MSButtonNode
+        
+        //Setup restart button selection handler
+        buttonRestart.selectedHandler = {
+            
+            //Grab reference to our spriteKit view
+            let skView = self.view as SKView!
+            
+            //Load Game scene
+            let scene = GameScene(fileNamed: "GameScene") as GameScene!
+            
+            //Ensure correct aspect mode
+            scene?.scaleMode = .aspectFill
+            
+            //Show debug
+            skView?.showsPhysics = true
+            skView?.showsDrawCount = true
+            skView?.showsFPS = false
+            
+            //Restart game scene
+            skView?.presentScene(scene)
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -36,9 +76,24 @@ class GameScene: SKScene {
         //Apply impulse to penguin
         penguin.avatar.physicsBody?.applyImpulse(force)
         
+        //Set camera to follow penguin
+        cameraTarget = penguin.avatar
+        
     }
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        
+        //Check we have a valid camera target to follow
+        if let cameraTarget = cameraTarget {
+            
+            //Set camera position to follow target horizontally, keep vertical locked
+            camera?.position = CGPoint(x: cameraTarget.position.x, y: camera!.position.y)
+            
+        }
+        
+        //Clamp camera scrolling to our visible scene area only
+        camera?.position.x.clamp(v1: 283, 677)
+        
     }
 }
